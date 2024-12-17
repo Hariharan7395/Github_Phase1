987a3fc59804bf228819b53b82d2576741a6657a
kishoreV85 <kishore.v@yantravision.com>
2024-12-17 15:40:02 +0530

    Include division

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include<stdio.h> //headerfile
 
 int main(){
-int number1=10;
-int number2=20;
+int number1=10;//sample value1
+int number2=20;//sample value2
 
 //Program to add
 int sum = 0;
@@ -16,9 +16,15 @@ int subtract=0;
 subtract = number1 -  number2;
 printf("The Difference is %d",subtract);
 
+//Program to multipy
 int multiply=0;
 multiply = number1 * number2;
 printf("The Multiplication of two numbers is %d",multiply);
+
+//Program to divide
+int Divide=0;
+Divide = number1 / number2;
+printf("The Division of two numbers is %d",Divide);
 
 return 0;
 }
