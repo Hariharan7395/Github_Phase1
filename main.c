582327e422c3f4f6404aba405aab0d50741f2f86
kishoreV85 <kishore.v@yantravision.com>
2024-12-17 15:03:22 +0530

    Created add program

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
+//Main created by Kishore.k
+
 #include<stdio.h> //headerfile
 
 int main(){
 int number1=10;
 int number2=20;
+
+//Program to add
+int sum = 0;
+sum = number1 + number2;
+printf("The sum is: %d",sum);
 
 return 0;
 }
