b2bbe2c4cde8f66a8b8da6d78c8aa411aa26bc78
Hariharan73958 <hariharan.s@yantravision.com>
2024-12-18 14:50:35 +0530

    changes made in Addition part

--- main.c
+++ main.c
@@ -5,26 +5,27 @@
 int main(){
 int number1=10;//sample value1
 int number2=20;//sample value2
+int number3=30;
 
 //Program to add
 int sum = 0;
-sum = number1 + number2;
-printf("The sum is: %d",sum);
+sum = number1 + number2+number3;
+printf("The sum is: %d\n",sum);
 
 //Program to Subtract
 int subtract=0;
 subtract = number1 -  number2;
-printf("The Difference is %d",subtract);
+printf("The Difference is %d\n",subtract);
 
 //Program to multipy
 int multiply=0;
 multiply = number1 * number2;
-printf("The Multiplication of two numbers is %d",multiply);
+printf("The Multiplication of two numbers is %d\n",multiply);
 
 //Program to divide
 int Divide=0;
 Divide = number1 / number2;
-printf("The Division of two numbers is %d",Divide);
+printf("The Division of two numbers is %d\n",Divide);
 
 return 0;
 }
