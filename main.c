b55e6d90af390c5ac8b7f07b3dfc4428c0069eda
kishoreV85 <kishore.v@yantravision.com>
2024-12-18 15:25:45 +0530

    Changes made in multiply

--- main.c
+++ main.c
@@ -5,26 +5,22 @@
 int main(){
 int number1=10;//sample value1
 int number2=20;//sample value2
-int number3=30;
+int number3=30;//sample value3
 
 //Program to add
-int sum = 0;
-sum = number1 + number2+number3;
+int sum=0;
+sum = number1 + number2 + number3;
 printf("The sum is: %d\n",sum);
 
 //Program to Subtract
 int subtract=0;
-<<<<<<< HEAD
+
 subtract = number1 -  number2;
 printf("The Difference is %d\n",subtract);
-=======
-subtract = number2 -  number1;
-printf("The Difference is %d",subtract);
->>>>>>> 496612affed7045b7bea2efc83c53061d5c22b3f
 
 //Program to multipy
 int multiply=0;
-multiply = number1 * number2;
+multiply = number1 * number2 * number3;
 printf("The Multiplication of two numbers is %d\n",multiply);
 
 //Program to divide
