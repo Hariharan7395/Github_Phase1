0811cd40b190ee2c78e9977e0381277c90c9b36b
Kishore02721 <kishore.k@yantravision.com>
2024-12-18 15:38:32 +0530

    message

--- main.c
+++ main.c
@@ -14,23 +14,9 @@ printf("The sum is: %d\n",sum);
 
 //Program to Subtract
 int subtract=0;
-<<<<<<< HEAD
-<<<<<<< HEAD
 subtract = number2 -  number1-0;
-=======
-<<<<<<< HEAD
 subtract = number1 -  number2;
 printf("The Difference is %d\n",subtract);
-=======
-subtract = number2 -  number1;
->>>>>>> 89a04257a49bf7befde7c50d17d955e32357fe11
-printf("The Difference is %d",subtract);
->>>>>>> 496612affed7045b7bea2efc83c53061d5c22b3f
-=======
-
-subtract = number1 -  number2;
-printf("The Difference is %d\n",subtract);
->>>>>>> 3cb04227182db2bca5e3be568941f02023b38b5f
 
 //Program to multipy
 int multiply=0;
@@ -42,6 +28,5 @@ int Divide=0;
 Divide = number1 / number2;
 printf("The Division of two numbers is %d\n",Divide);
 
-printf("Hello");
 return 0;
 }
